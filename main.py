618d794ca23714ca6affa8750463f611a2843488
shinyoung.joo <bit1010@live.com>
2023-08-15 22:18:17 +0900

    audio byte데이터를 html로 전달시 base64로 인코딩해서 utf-8로 전달하도록 수정 및 코드 정리

--- main.py
+++ main.py
@@ -2,31 +2,29 @@
 
 import os
 from io import BytesIO
+import base64
 from flask import Flask, render_template
 from flask import Flask, request, Response
 from gtts import gTTS
 
 DEFAULT_LANG = os.getenv('DEFAULT_LANG', 'ko')
-
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
 
-    return render_template('index.html', image_file="david.jpg")
-
-
-@app.route("/hello")
-def hello():
+    text = '안녕하세요. 인공지능 로봇 David입니다. 무엇을 도와 드릴까요?'
 
     lang = request.args.get('lang', DEFAULT_LANG)
     fp = BytesIO()
-    text = '안녕하세요. 인공지능 로봇 David입니다. 무엇을 도와 드릴까요?'
     gTTS(text, "com", lang).write_to_fp(fp)
 
-    # return Response(fp.getvalue(), mimetype='audio/mpeg')
-    return render_template('audio.html', audiodata=fp.getvalue())
+    # base64로 인코딩해서 utf-8로 html페이지에 전달
+    encoded_audio_data = base64.b64encode(fp.getvalue())
+
+    # return Response(fp.getvalue(), mimetype='audio/mpeg') # 페이지 전달없이 바로 재생
+    return render_template('index.html', image_file="david.jpg", audiodata=encoded_audio_data.decode('utf-8'))
 
 
 if __name__ == '__main__':
